b12d9960fddf7bfefe649a48b390e894fc9a8c1a
nabsabt <nabsabt@gmail.com>
2026-01-23 13:59:11 +0100

    chat service implemented

--- src/app/@Component/modal/chat/chat.component.ts
+++ src/app/@Component/modal/chat/chat.component.ts
@@ -1,10 +1,14 @@
 import {
+  AfterViewChecked,
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   inject,
   OnDestroy,
   OnInit,
   signal,
+  ViewChild,
 } from '@angular/core';
 import { AlertService } from '../../../@Service/alert.service';
 import { ChatService } from '../../../@Service/chat.service';
@@ -14,16 +18,19 @@ import { AI_CHAR } from '../../../@Interface/maproot.interface';
 import { MatSelectModule } from '@angular/material/select';
 import { Subscription } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'chat',
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
-  imports: [TranslateModule, MatSelectModule, ReactiveFormsModule],
+  imports: [TranslateModule, MatSelectModule, ReactiveFormsModule, CommonModule],
   providers: [AlertService, ChatService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatComponent implements OnInit, OnDestroy {
+export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('chatBox') chatBox?: ElementRef<HTMLElement>;
+
   private sendMessageSub: Subscription;
 
   private chatService = inject(ChatService);
@@ -36,12 +43,12 @@ export class ChatComponent implements OnInit, OnDestroy {
   ]);
   public selectedCharacter = signal<AI_CHAR | undefined>(undefined);
   public messageInputText = signal<string>('');
-  public messages = signal<Array<string>>([]);
+  public messages = signal<Array<{ isResponse: boolean; text: string }>>([]);
 
   public loading = signal<boolean>(false);
 
   form = new FormGroup({
-    message: new FormControl('', [
+    message: new FormControl({ value: '', disabled: this.loading() }, [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(400),
@@ -49,7 +56,19 @@ export class ChatComponent implements OnInit, OnDestroy {
   });
 
   constructor() {}
+  ngAfterViewInit(): void {
+    this.scrollToBottom();
+  }
+
   ngOnInit(): void {}
+
+  private scrollToBottom() {
+    requestAnimationFrame(() => {
+      const el = this.chatBox?.nativeElement;
+      if (!el) return;
+      el.scrollTop = el.scrollHeight - el.clientHeight;
+    });
+  }
 
   public onCharacterSelected(char: AI_CHAR) {
     this.selectedCharacter.set(char);
@@ -60,14 +79,20 @@ export class ChatComponent implements OnInit, OnDestroy {
       return;
     }
     const message: string = this.form.getRawValue().message as string;
-    console.log(message, this.selectedCharacter());
     this.loading.set(true);
+    this.messages.update((arr) => [...arr, { isResponse: false, text: message }]);
+    this.scrollToBottom();
+
+    this.messageInputText.set('');
+    this.form.controls.message.setValue('');
+
     this.sendMessageSub = this.chatService
       .postChatMessage(message, this.selectedCharacter() as AI_CHAR)
       .subscribe({
         next: (res: { reply: string }) => {
-          console.log('reply: ', res.reply);
+          this.messages.update((arr) => [...arr, { isResponse: true, text: res.reply }]);
           this.loading.set(false);
+          this.scrollToBottom();
         },
         error: (error: HttpErrorResponse): HttpErrorResponse => {
           this.loading.set(false);
@@ -79,5 +104,6 @@ export class ChatComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.selectedCharacter.set(undefined);
     this.sendMessageSub?.unsubscribe();
+    this.messages.set([]);
   }
 }
